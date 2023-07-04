57e366aa3f4e288c40d337bc8563120386c44aaa
nolate26 <nicolas.olate@uc.cl>
2023-07-03 21:06:17 -0400

    repositorio

---
+++ ataque-SJ/src/game/Board.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Board.css';
+
+
+function Board() {
+  return (
+    <img src='../public/assets/img/board.jpg' alt="Tablero" className="Board" />
+  );
+
+}
+
+export default Board;
